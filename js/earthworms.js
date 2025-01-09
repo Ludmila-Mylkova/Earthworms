6c4cd5708f5f5282fa7fd78d2b6680f94738a8a7
michal-aldor-mylek <80908016+michal-aldor-mylek@users.noreply.github.com>
2025-01-09 20:02:54 +0100

    Add files via upload
    Initial blueprint for my daughter's HTML JS game

---
+++ js/earthworms.js
@@ -0,0 +1,25 @@
+    // JavaScript code goes here, implementing game logic
+    // Generic syntax : https://www.w3schools.com/js/js_syntax.asp
+    // 1, learn how to use window.addEventListener and register keyboard keys being held down
+    //        https://www.w3schools.com/jsref/met_win_addeventlistener.asp
+    // 2, find out how to modify image data each render loop to draw moving earthworm(s)
+    //        https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+    //        https://developer.mozilla.org/en-US/docs/Web/API/ImageData/data
+    // 3, create and polish game logic
+    // -- earthworm angle of movement can be controlled by keyboard
+    // -- earthworm collision (use image data to look if there is obstacle in next pixel "step")
+    // -- movement speed gradually increase
+    // -- add multiplayer (second earthworm)
+    // -- add possibility to configure them (colors)
+    // 4, presentation layer
+    // -- main page graphics / layout
+    // -- sounds or music?
+    // 5, continue implementing ideas to have greatest possible game
+
+    function start_game() {
+        // Write your code here. It will be called when Start button is clicked.
+        // Remember to use functions to divide code into separate logical blocks
+        // Use variables and name them in a descriptive way
+        // Use Git to save and version your work
+    }
+
